74f68c2c28c444afa230d2a1fc0796f95f14bba3
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-15 16:25:45 +0000

    Getting the chat functionality working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+import './App.css'
+require('dotenv').config()
 
-function App() {
-  return <div>Chat App.</div>
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={process.env.REACT_APP_projectID}
+      userName={process.env.REACT_APP_userName}
+      userSecret={process.env.REACT_APP_userSecret}
+    />
+  )
 }
+
 export default App
