50c0edb7acf72eb78e3d99763073879bac22d31c
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-17 16:43:10 +0000

    MyMessage, TheirMessage

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -19,7 +19,14 @@ const ChatFeed = (props) => {
       return (
         <div key={`msg_${index}`} style={{ width: '100%' }}>
           <div className="message-block">
-            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+            {isMyMessage ? (
+              <MyMessage message={message} />
+            ) : (
+              <TheirMessage
+                message={message}
+                lastMessage={messages[lastMessageKey]}
+              />
+            )}
           </div>
           <div
             className="read-receipts"
@@ -34,9 +41,23 @@ const ChatFeed = (props) => {
     })
   }
 
-  renderMessages()
+  if (!chat) return 'Loading...'
 
-  return <div>ChatFeed</div>
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  )
 }
 
 export default ChatFeed
